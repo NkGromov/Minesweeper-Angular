746dfc2e5477f5e02297414709fcc3ab0c268f3b
nikita gromov <gromoff332@gmail.com>
2022-04-26 14:11:29 +0500

    deleted row component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SapperCellComponent } from './components/sapper-cell/sapper-cell.component';
-import { SapperRowComponent } from './components/sapper-row/sapper-row.component';
 import { SapperTableComponent } from './components/sapper-table/sapper-table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SapperTableComponent,
-    SapperRowComponent,
-    SapperCellComponent,
-  ],
+  declarations: [AppComponent, SapperTableComponent, SapperCellComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
